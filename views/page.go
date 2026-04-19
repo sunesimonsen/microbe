2e82818013c79b1cedf06ef6ea97dbd53a5cd43f
Sune Simonsen <sune@we-knowhow.dk>
2026-04-19 20:36:48 +0200

    Add page view with full HTML document structure

---
+++ views/page.go
@@ -0,0 +1,37 @@
+package views
+
+import (
+	. "maragu.dev/gomponents"
+	. "maragu.dev/gomponents/html"
+)
+
+func Page(title string, children ...Node) Node {
+	return Doctype(
+		HTML(
+			Lang("en"),
+			Head(
+				TitleEl(Text(title)),
+				Meta(Charset("UTF-8")),
+				Meta(
+					Name("viewport"),
+					Content("width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=1"),
+				),
+				Meta(Name("apple-mobile-web-app-capable"), Content("yes")),
+				Meta(Name("description"), Content("Microbe.css documentation"), Lang("en")),
+				Link(Rel("icon"), Type("image/png"), Href("/assets/favicon-96x96.png"), Attr("sizes", "96x96")),
+				Link(Rel("icon"), Type("image/svg+xml"), Href("/assets/favicon.svg")),
+				Link(Rel("shortcut icon"), Href("/assets/favicon.ico")),
+				Link(Rel("apple-touch-icon"), Attr("sizes", "180x180"), Href("/assets/apple-touch-icon.png")),
+				Meta(Name("apple-mobile-web-app-title"), Content("Microbe")),
+				Link(Rel("manifest"), Href("/assets/site.webmanifest")),
+				Link(Rel("stylesheet"), Type("text/css"), Href("/assets/microbe.css")),
+				Link(Rel("stylesheet"), Type("text/css"), Href("/assets/page.css")),
+				Script(Src("/assets/microbe.js")),
+				Script(Src("/assets/swap-it.js")),
+			),
+			Body(
+				Group(children),
+			),
+		),
+	)
+}
