371e70d8c9d9aa1ca544088947ba9b9e3a1905c1
Sune Simonsen <sune@we-knowhow.dk>
2026-04-19 20:39:09 +0200

    Add buttons view

---
+++ views/buttons.go
@@ -0,0 +1,55 @@
+package views
+
+import (
+	. "maragu.dev/gomponents"
+	. "maragu.dev/gomponents/html"
+)
+
+func Buttons() Node {
+	return Group([]Node{
+		HGroup(
+			ID("content-header"),
+			H1(Text("Buttons")),
+		),
+		Div(
+			ID("content"),
+			Attr("role", "document"),
+			Div(
+				Class("buttons-row"),
+				Button(Class("solid"), Text("Solid Button")),
+				Button(Class("outline"), Text("Outline Button")),
+				Button(Class("ghost"), Text("Ghost Button")),
+			),
+			Div(
+				Class("buttons-row"),
+				Button(Class("solid"), Text("Solid Button")),
+				Button(Class("outline"), Text("Outline Button")),
+				Button(Class("ghost"), Text("Ghost Button")),
+			),
+			Div(
+				Class("buttons-row"),
+				Button(Class("solid"), Disabled(), Text("Solid Button")),
+				Button(Class("outline"), Disabled(), Text("Outline Button")),
+				Button(Class("ghost"), Disabled(), Text("Ghost Button")),
+			),
+			Div(
+				Class("buttons-row"),
+				Button(Class("solid"), gearIcon(), Text(" Solid Button")),
+				Button(Class("outline"), gearIcon(), Text(" Outline Button")),
+				Button(Class("ghost"), gearIcon(), Text(" Ghost Button")),
+			),
+			Div(
+				Class("buttons-row"),
+				Button(Class("solid"), Text("Solid Button "), gearIcon()),
+				Button(Class("outline"), Text("Outline Button "), gearIcon()),
+				Button(Class("ghost"), Text("Ghost Button "), gearIcon()),
+			),
+			Div(
+				Class("buttons-row"),
+				Button(Class("solid icon"), burgerIcon()),
+				Button(Class("outline icon"), burgerIcon()),
+				Button(Class("ghost icon"), burgerIcon()),
+			),
+		),
+	})
+}
