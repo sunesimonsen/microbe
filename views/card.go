7a7ff6c4f3fb66e6d18483582c5b78295e1fc66a
Sune Simonsen <sune@we-knowhow.dk>
2026-06-09 11:37:42 +0200

    Render card examples as all other examples

--- views/card.go
+++ views/card.go
@@ -1,58 +1,46 @@
 package views
 
 import (
-	"github.com/iancoleman/strcase"
 	. "maragu.dev/gomponents"
 	. "maragu.dev/gomponents/html"
 )
-
-func exampleCard(name string, parts ...Node) pageSection {
-	return pageSection{
-		name: name,
-		content: Article(
-			ID(strcase.ToKebab(name)),
-			Class("card"),
-			Group(parts),
-		),
-	}
-}
 
 func CardView() Node {
 	cardText := "Vestibulum venenatis neque nec iaculis viverra. Proin a odio ex. Etiam eget rutrum nulla, id finibus tellus. Proin dignissim tortor lacus, in vehicula arcu commodo ut. Aenean quis euismod lectus, vitae ornare ex. Vivamus posuere lectus at quam maximus semper. Fusce sagittis in est a consequat."
 
 	return docpage(
 		HGroup(H1(Text("Card"))),
-		exampleCard(
-			"bare card",
-			Section(
-				H5(Text("Bare card")),
-				P(Text(cardText)),
+		example(
+			"Bare card",
+			Article(
+				Class("card"),
+				Section(P(Text(cardText))),
 			),
 		),
-		exampleCard(
+		example(
 			"Card with header",
-			Header(Text("Header")),
-			Section(
-				H5(Text("Card with header")),
-				P(Text(cardText)),
+			Article(
+				Class("card"),
+				Header(Text("Header")),
+				Section(P(Text(cardText))),
 			),
 		),
-		exampleCard(
+		example(
 			"Card with footer",
-			Section(
-				H5(Text("Card with footer")),
-				P(Text(cardText)),
+			Article(
+				Class("card"),
+				Section(P(Text(cardText))),
+				Footer(Text("Footer")),
 			),
-			Footer(Text("Footer")),
 		),
-		exampleCard(
+		example(
 			"Card with header and footer",
-			Header(Text("Header")),
-			Section(
-				H5(Text("Card with header and footer")),
-				P(Text(cardText)),
+			Article(
+				Class("card"),
+				Header(Text("Header")),
+				Section(P(Text(cardText))),
+				Footer(Text("Footer")),
 			),
-			Footer(Text("Footer")),
 		),
 	)
 }
