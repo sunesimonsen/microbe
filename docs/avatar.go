7ca8757e3e7030fb108b77b41be5942c53e14f05
Sune Simonsen <sune@we-knowhow.dk>
2026-08-29 17:47:19 +0200

    Use a figure for the image avatar

--- docs/avatar.go
+++ docs/avatar.go
@@ -28,7 +28,9 @@ var AvatarPage = NewPage(
         <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
       </svg>
     </figure>
-    <img class="avatar" src="https://microbe.sune.one/assets/user.png" alt="User avatar">
+    <figure class="avatar" aria-label="User avatar">
+      <img src="https://microbe.sune.one/assets/user.png" alt="User avatar">
+    </figure>
     <figure class="avatar" aria-label="Sune Simonsen">
       <span aria-hidden="true">SS</span>
     </figure>
